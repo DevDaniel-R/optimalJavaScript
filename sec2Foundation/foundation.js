96d7a9ad7870c462182dac6f45e165b42afb043c
Daniel Rodriguez <drodriguez661@gmail.com>
2020-10-10 22:14:48 -0500

    first commit

---
+++ sec2Foundation/foundation.js
@@ -0,0 +1,11 @@
+// inline caching
+function fincUser(user) {
+  return `found ${user.firstName} ${user.lastName}`
+}
+
+const userData {
+  firstName: 'Johnson'
+  lastName: 'junior'
+}
+
+findUser(userData)

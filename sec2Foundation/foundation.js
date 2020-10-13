b1c671660d70c58319c710c4c723e694820c049b
Daniel Rodriguez <drodriguez661@gmail.com>
2020-10-12 21:49:32 -0500

    Memory Heap

--- sec2Foundation/foundation.js
+++ sec2Foundation/foundation.js
@@ -1,5 +1,5 @@
-// inline caching
-function fincUser(user) {
+// inline caching (Optimizing code) - cache so that findUser just becomes that piece of text (findUser)
+function findUser(user) {
   return `found ${user.firstName} ${user.lastName}`
 }
 
@@ -9,3 +9,42 @@ const userData {
 }
 
 findUser(userData)
+
+
+
+// hidden classes - internally slow things down - to prevent this we should assign all properties of an objects to its constructor
+
+function Animal(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+const obj1 = new Animal(1,2);
+const obj2 = new Animal(3,4);
+
+// obj1.a = 30;
+// obj1.b = 100;
+// obj2.b = 30;
+// obj2.a = 100;
+
+delete obj1.x = 30;
+
+
+//Call stack + Memory Heap
+const number = 610; // allocate memory for number
+const string = 'some text' //allocate memory for a string
+const human = { //allocate memory for an object... and it's values
+first: 'Daniel',
+last : 'Rodriguez'
+}
+
+function subtractTwo(num) {
+  return num - 2;
+}
+
+function calculate() {
+  const sumTotal = 4 + 5;
+  return subtractTwo(sumTotal);
+}
+
+calculate();
